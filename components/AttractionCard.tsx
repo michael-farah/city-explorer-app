c840f9464f47b2209710015e6ec912254d372bc9
Catherine Pulman <159820305+Clatherine@users.noreply.github.com>
2024-07-04 15:17:45 +0100

    updated styling on attractionCard component

--- components/AttractionCard.tsx
+++ components/AttractionCard.tsx
@@ -9,54 +9,62 @@ import { RotateInDownLeft } from "react-native-reanimated";
 export default function AttractionCard({ navigation, cityName, attraction }) {
   const { user, setUser } = useContext(UserContext);
   const [photo, setPhoto] = useState("");
+const [attractionType, setAttractionType] = useState()
 
   useEffect(() => {
     getPhoto(attraction.photos[0].name, 1000, 1000).then((response) => {
       setPhoto(response);
+      console.log(attraction, 'attraction')
+if (attraction.primaryTypeDisplayName){
+  console.log(attraction.primaryTypeDisplayName, 'attraction.primaryTypeDisplayName')
+  setAttractionType(attraction.primaryTypeDisplayName.text)
+}
+else{
+    const attractionType0 = attraction.types[0]
+    console.log(attractionType0)
+    setAttractionType(attractionType0)
+}
     });
   }, []);
+
+
   const seeMoreClick = ({ attraction }) => {
     navigation.navigate("Attraction", { attraction });
   };
   const bucketListClick = ({ attraction }) => {
     postBucketListItem(attraction, user.username, cityName);
-    // add a way to show the user that they have clicked the button and its working/complete/failed
-    console.log("item added to bucket list");
   };
 
   return (
     <View style={styles.container}>
       <View style={styles.attractionTitle}>
-        <Text style={styles.titleText}>{attraction.displayName.text}</Text>
+        <View> <Text style={styles.titleText}>{attraction.displayName.text}</Text></View>
+    
       </View>
       <View style={styles.mainBody}>
         <View style={styles.imageBox}>
           <Image style={styles.image} source={{ uri: photo }} />
         </View>
         <View style={styles.textBody}>
+        {attraction.editorialSummary ? (
+            <Text style={styles.editorialSummary}>{attraction.editorialSummary.text}</Text>
+          ) : null}
           <Text style={styles.rating}>
             Average rating of {attraction.rating} according to{" "}
             {attraction.userRatingCount} reviewers
           </Text>
-          {attraction.editorialSummary ? (
-            <Text style={styles.editorialSummary}>{attraction.editorialSummary.text}</Text>
-          ) : null}
-          <Text
-            style={{ color: "blue" }}
-            onPress={() => Linking.openURL(attraction.websiteUri)}
-          >
-            Visit official site
-          </Text>
-          <Text>Categories: {attraction.types.join(", ")}</Text>
+          {/* <Text>Categories: {attraction.types.join(", ")}</Text>
+           */}
+<Text style={styles.category}>Category: {attractionType}</Text>
           <View style={styles.buttonContainer}>
-        <View>
+        <View style={styles.button}>
           <Button
             title="See more details"
             onPress={() => seeMoreClick({ attraction })}
             disabled={false}
           />
         </View>
-        <View>
+        <View style={styles.button}>
           <Button
             title="Add to Bucket List"
             onPress={() => bucketListClick({ attraction })}
@@ -75,46 +83,67 @@ const styles = StyleSheet.create({
   container: { 
     margin: 10,
      flex: 1 ,
-    borderWidth: 5},
+    borderWidth: 5,
+    padding: 10
+  },
   image: {
     width: 150,
     height: 150,
     borderRadius: 10
   },
-  attractionTitle: {
-  },
+ 
   titleText: {
     fontWeight: "bold",
-    borderWidth: 3,
-    borderColor: "green",
-    padding:10
+    fontSize: 20,
+    marginBottom: 10
+ 
   },
   mainBody: {
     flexDirection: "row",
-    borderWidth: 3,
-    borderColor: "red"
+    // borderWidth: 3,
+    // borderColor: "red",
+    flexWrap: "wrap",
+    flex:1
   },
   textBody:{
-    flex:1,
+    flex:3,
     flexWrap: "wrap",
-    borderWidth: 3,
-    borderColor: "purple",
-    padding: 10
+    // borderWidth: 3,
+    // borderColor: "purple",
+    paddingHorizontal: 20, 
+    minWidth: 200,
+    height: "auto", 
+
   }
   ,
   buttonContainer: {
-    flex: 1,
     flexDirection: "row",
+    flexWrap: "wrap",
+    marginTop: 10,
+    justifyContent: "flex-start",
+    alignContent: "flex-end"
   },
   editorialSummary: {
     flex:1,
-    marginVertical: 10
+    marginVertical: 10,
+    width: "100%"
   },
   imageBox: {
-    padding: 10,
-   
+    // padding: 10,
+    justifyContent: "center",
+    
+    flex: 1,
+    height: "auto"
   },
   rating: {
-    marginBottom: 10
+    marginBottom: 10,
+    width: "100%"
+  },
+  category:{
+    width: "100%"
+  },
+  button:{
+    margin: 3,
+    justifyContent: "center"
   }
 });
