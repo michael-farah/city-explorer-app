0bf6d7e10aed8d1609749dc9af7e8aa2007801ef
Catherine Pulman <159820305+Clatherine@users.noreply.github.com>
2024-07-04 17:05:48 +0100

    added accessibility toggle to filter attraction list to only show wheelchair-accessible places

--- components/SearchPage.tsx
+++ components/SearchPage.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet , Text} from "react-native";
+import { StyleSheet , Text, CheckBox, View} from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 import ParallaxScrollView from "@/components/ParallaxScrollView";
 import { ThemedText } from "@/components/ThemedText";
@@ -9,20 +9,41 @@ import { useState, useEffect } from "react";
 import { getAttractions, getCities, getCity } from "@/app/api";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { checkIfConfigIsValid } from "react-native-reanimated/lib/typescript/reanimated2/animation/springUtils";
 
 export default function SearchPage({navigation}) { 
 const [cityName, setCityName] = useState("London")
 const [attractions, setAttractions] = useState([])
 const [attractionsListIsLoading, setAttractionsListIsLoading] = useState(true);
+const [accessibleOnly, setAccessibleOnly] = useState(false)
+const [fullCityList, setFullCityList] = useState([])
 
 useEffect(()=>{
         getCity(cityName).then((response)=>{
         getAttractions(response.city.city_longitude, response.city.city_latitude, response.city.city_radius)
         .then((response)=>{
           setAttractionsListIsLoading(false)
-          setAttractions(response.data.places)})
+          setAttractions(response.data.places)
+          setFullCityList(response.data.places)})
     }).catch((err) => {console.log(err)})
 },[cityName])
+
+useEffect(()=>{
+if(accessibleOnly){
+  setAttractions((attractions)=>{
+const copyAttractions = [...attractions]
+const filteredAttrations = copyAttractions.filter((attraction)=>{ 
+  return attraction.accessibilityOptions.wheelchairAccessibleEntrance && attraction.accessibilityOptions.wheelchairAccessibleRestroom 
+})
+return filteredAttrations
+  })
+}
+else{
+    setAttractions(fullCityList)
+}
+}
+, [accessibleOnly])
+
 return (
    
     <ParallaxScrollView
@@ -37,6 +58,13 @@ return (
         </ThemedText>
       </ThemedView>
       <CityDropdown setCityName={setCityName} />
+      <View style={styles.accessibilityCheckboxContainer}>
+      <CheckBox
+          value={accessibleOnly}
+          onValueChange={setAccessibleOnly}
+          style={styles.checkbox}
+        />
+         <Text style={styles.label}>Wheelchair-accessible attractions only (has a wheelchair-accessible entrance and toilet)</Text></View>
       {attractionsListIsLoading? <Text>Attractions list is loading ...</Text>: <AttractionsList cityName={cityName} attractions={attractions} navigation={navigation}/>}
     </ParallaxScrollView>
  
@@ -54,5 +82,15 @@ const styles = StyleSheet.create({
       flexDirection: "row",
       gap: 8,
     },
+    accessibilityCheckboxContainer: {
+      flexDirection: 'row',
+      marginBottom: 20,
+    },
+    label: {
+      margin: 8,
+    },
+    checkbox:{
+      alignSelf: 'center',
+    }
   });
   
